122e99dea040254d56042a198e2c8627eed8c0c5
MariJ12 <mari.munizj@gmail.com>
2024-01-26 17:42:02 +0000

    added btn back for test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">HELLO MJ</header>
+      <a href="#" className="btn btn-primary">
+        Hi Gorgeous
+      </a>
     </div>
   );
 }
