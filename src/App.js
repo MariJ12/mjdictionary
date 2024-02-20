274d5fe30af7fc8084904455b0fff29efaae70cc
MariJ12 <mari.munizj@gmail.com>
2024-02-20 10:17:32 +0000

    added logo to header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import mwebb from "./mwebb.jpg";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">HELLO MJ</header>
+      <header className="App-header">
+        <img src={mwebb} className="Mwebb-logo img-fluid" alt="mwebblogo" />
+      </header>
       <a rel="noreferrer" href="/" className="btn btn-primary">
         Hi Gorgeous
       </a>
     </div>
   );
 }
-
-export default App;
