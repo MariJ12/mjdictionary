dad8ce4c6db666c818d1365492d166edce124e82
MariJ12 <mari.munizj@gmail.com>
2024-02-20 11:29:41 +0000

    added Dictionary Search Engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import mwebb from "./mwebb.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={mwebb} className="Mwebb-logo img-fluid" alt="mwebblogo" />
-      </header>
-      <a rel="noreferrer" href="/" className="btn btn-primary">
-        Hi Gorgeous
-      </a>
+      <div className="container">
+        <header className="App-header">
+          <img src={mwebb} className="Mwebb-logo img-fluid" alt="mwebblogo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Coded by MJ</small>
+        </footer>
+      </div>
     </div>
   );
 }
